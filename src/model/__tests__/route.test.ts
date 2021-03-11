1aebfc29498c025321fe8e6977b7afa33cbc6582
KubiGR <adranidisb@gmail.com>
2021-03-11 22:23:09 +0200

    Fix jest

--- src/model/__tests__/route.test.ts
+++ src/model/__tests__/route.test.ts
@@ -1,23 +1,22 @@
-import { City } from "model/city";
-import { Connection } from "model/connection";
-import { Route } from "model/route";
-import { TrackColor } from "model/trackColor";
+import { City } from 'model/city';
+import { Connection } from 'model/connection';
+import { Route } from 'model/route';
+import { TrackColor } from 'model/trackColor';
 
 test('route1', () => {
-    let from = new City("from");
-    let to = new City("to");
-    let connection = new Connection(from, to, 3, TrackColor.Red);    
+    let from = new City('from');
+    let to = new City('to');
+    let connection = new Connection(from, to, 3, TrackColor.Red);
     let route = new Route([connection]);
 
     expect(route.actionLength()).toBe(1);
-  });
-
+});
 
 test('route', () => {
-    let from = new City("from");
-    let to = new City("to");
-    let connection = new Connection(from, to, 3, TrackColor.Red);    
+    let from = new City('from');
+    let to = new City('to');
+    let connection = new Connection(from, to, 3, TrackColor.Red);
     let route = new Route([connection]);
 
     expect(route.trainLength()).toBe(3);
-  });
+});
