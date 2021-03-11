9029e9d0942dc54291bfd89528bb84baad436b90
D Dranidis <dranidis@gmail.com>
2021-03-12 00:41:04 +0200

    Fix lint

--- src/model/__tests__/route.test.ts
+++ src/model/__tests__/route.test.ts
@@ -13,46 +13,46 @@ test('route1', () => {
 });
 
 test('singleRouteTrainLength', () => {
-    let from = new City("from");
-    let to = new City("to");
-    let connection = new Connection(from, to, 3, TrackColor.Red);    
-    let route = new Route([connection]);
+  const from = new City('from');
+  const to = new City('to');
+  const connection = new Connection(from, to, 3, TrackColor.Red);
+  const route = new Route([connection]);
 
-    expect(route.trainLength()).toBe(3);
-  });
+  expect(route.trainLength()).toBe(3);
+});
 
 test('doubleRouteLength', () => {
-  let from = new City("from");
-  let to = new City("to");
-  let final = new City("final");
-  let connection1 = new Connection(from, to, 3, TrackColor.Red);    
-  let connection2 = new Connection(to, final, 4, TrackColor.Black);    
-  let route = new Route([connection1, connection2]);
+  const from = new City('from');
+  const to = new City('to');
+  const final = new City('final');
+  const connection1 = new Connection(from, to, 3, TrackColor.Red);
+  const connection2 = new Connection(to, final, 4, TrackColor.Black);
+  const route = new Route([connection1, connection2]);
 
   expect(route.actionLength()).toBe(2);
 });
 
 test('doubleRouteTrainLength', () => {
-  let from = new City("from");
-  let to = new City("to");
-  let final = new City("final");
-  let connection1 = new Connection(from, to, 3, TrackColor.Red);    
-  let connection2 = new Connection(to, final, 4, TrackColor.Black);    
-  let route = new Route([connection1, connection2]);
+  const from = new City('from');
+  const to = new City('to');
+  const final = new City('final');
+  const connection1 = new Connection(from, to, 3, TrackColor.Red);
+  const connection2 = new Connection(to, final, 4, TrackColor.Black);
+  const route = new Route([connection1, connection2]);
 
   expect(route.trainLength()).toBe(7);
 });
 
 test('invalidRoute', () => {
-  let from = new City("from");
-  let to1 = new City("to1");
-  let to2 = new City("to2");
-  let final = new City("final");
-  let connection1 = new Connection(from, to1, 3, TrackColor.Red);    
-  let connection2 = new Connection(to2, final, 4, TrackColor.Black);    
-  
+  const from = new City('from');
+  const to1 = new City('to1');
+  const to2 = new City('to2');
+  const final = new City('final');
+  const connection1 = new Connection(from, to1, 3, TrackColor.Red);
+  const connection2 = new Connection(to2, final, 4, TrackColor.Black);
+
   expect(() => {
-    let route = new Route([connection1, connection2]);
+    const route = new Route([connection1, connection2]);
   }).toThrow();
 });
 
--- src/model/route.ts
+++ src/model/route.ts
@@ -1,24 +1,30 @@
 import { Connection } from './connection';
 
 export class Route {
-    trainLength(): number {
-        return this.connections.map((c: Connection) => c.length).reduce((acc, cur) => acc + cur);
-    }
-    actionLength(): number {
-        return this.connections.length;
-    }
-    connections: Connection[];
+  trainLength(): number {
+    return this.connections
+      .map((c: Connection) => c.length)
+      .reduce((acc, cur) => acc + cur);
+  }
+  actionLength(): number {
+    return this.connections.length;
+  }
+  connections: Connection[];
 
-    constructor(connections: Connection[]) {
-        this.connections = connections;
-        this.checkConnections();
-    }
+  constructor(connections: Connection[]) {
+    this.connections = connections;
+    this.checkConnections();
+  }
 
-    checkConnections() {
-        for (let i = 0; i < this.connections.length - 1; i++) {
-            if (this.connections[i].to.name != this.connections[i + 1].from.name)
-                throw new Error('Line is not continuous:' + this.connections[i].to.name + " neq " + this.connections[i + 1].from.name);
-        }
+  checkConnections(): void {
+    for (let i = 0; i < this.connections.length - 1; i++) {
+      if (this.connections[i].to.name != this.connections[i + 1].from.name)
+        throw new Error(
+          'Line is not continuous:' +
+            this.connections[i].to.name +
+            ' neq ' +
+            this.connections[i + 1].from.name,
+        );
     }
+  }
 }
-
