e1ab9c8bb81c3bb2d4e9dadd0879168851eb6c69
D Dranidis <dranidis@gmail.com>
2021-03-11 22:33:47 +0200

    Fix conflict

--- src/model/__tests__/route.test.ts
+++ src/model/__tests__/route.test.ts
@@ -12,11 +12,35 @@ test('route1', () => {
     expect(route.actionLength()).toBe(1);
 });
 
-test('route', () => {
-    let from = new City('from');
-    let to = new City('to');
-    let connection = new Connection(from, to, 3, TrackColor.Red);
+test('singleRouteTrainLength', () => {
+    let from = new City("from");
+    let to = new City("to");
+    let connection = new Connection(from, to, 3, TrackColor.Red);    
     let route = new Route([connection]);
 
     expect(route.trainLength()).toBe(3);
+  });
+
+
+test('doubleRouteLength', () => {
+  let from = new City("from");
+  let to = new City("to");
+  let final = new City("final");
+  let connection1 = new Connection(from, to, 3, TrackColor.Red);    
+  let connection2 = new Connection(to, final, 4, TrackColor.Black);    
+  let route = new Route([connection1, connection2]);
+
+  expect(route.actionLength()).toBe(2);
+});
+
+
+test('doubleRouteTrainLength', () => {
+  let from = new City("from");
+  let to = new City("to");
+  let final = new City("final");
+  let connection1 = new Connection(from, to, 3, TrackColor.Red);    
+  let connection2 = new Connection(to, final, 4, TrackColor.Black);    
+  let route = new Route([connection1, connection2]);
+
+  expect(route.trainLength()).toBe(7);
 });
