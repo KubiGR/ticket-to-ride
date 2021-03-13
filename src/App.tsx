32b99e9614c307b66d2ee5805034eb7a9b3fc3b9
KubiGR <adranidisb@gmail.com>
2021-03-13 02:41:38 +0200

    Trying resize logic for city pointers

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,14 @@ const App = (): JSX.Element => {
   // const [width, setWidth] = useState(0);
   const ref = useRef<HTMLDivElement>(null);
   const { width, height } = useResizeObserver<HTMLDivElement>({ ref });
+  // aspect ratio of usaMap = 1.56
+  const mapWidth = 624;
+  const mapHeight = 400;
+  const vancouverPos = {
+    x: 0.08,
+    y: 0.11,
+    s: 0.008,
+  };
   // console.log(targetRef.current?.clientWidth);
 
   // useEffect(() => {
@@ -35,12 +43,17 @@ const App = (): JSX.Element => {
   return (
     <div ref={ref}>
       Size: {width}x{height}
-      <Stage width={width} height={window.innerHeight}>
-        <Layer zIndex={1}>
-          <Map width={width} height={window.innerHeight} />
+      <Stage width={mapWidth} height={mapHeight}>
+        <Layer>
+          <Map width={mapWidth} height={mapHeight} />
         </Layer>
-        <Layer zIndex={2} onClick={() => console.log('click')}>
-          <Circle x={121} y={115} radius={10} fill="green" />
+        <Layer onClick={() => console.log('click')}>
+          <Circle
+            x={mapWidth * vancouverPos.x}
+            y={mapHeight * vancouverPos.y}
+            radius={mapWidth * vancouverPos.s}
+            fill="green"
+          />
         </Layer>
       </Stage>
     </div>
