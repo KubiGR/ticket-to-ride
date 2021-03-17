34212d0faf8f26c4e61468a92c17ea92a49fcd15
D Dranidis <dranidis@gmail.com>
2021-03-17 16:53:07 +0200

    Remove City

--- src/model/__tests__/route.test.ts
+++ src/model/__tests__/route.test.ts
@@ -1,55 +1,40 @@
-import { City } from 'model/city';
 import { Connection } from 'model/connection';
 import { Route } from 'model/route';
 import { TrackColor } from 'model/trackColor';
 
 test('route1', () => {
-  const from = new City('from');
-  const to = new City('to');
-  const connection = new Connection(from.name, to.name, 3, TrackColor.Red);
+  const connection = new Connection('from', 'to', 3, TrackColor.Red);
   const route = new Route([connection]);
 
   expect(route.actionLength()).toBe(1);
 });
 
 test('singleRouteTrainLength', () => {
-  const from = new City('from');
-  const to = new City('to');
-  const connection = new Connection(from.name, to.name, 3, TrackColor.Red);
+  const connection = new Connection('from', 'to', 3, TrackColor.Red);
   const route = new Route([connection]);
 
   expect(route.trainLength()).toBe(3);
 });
 
 test('doubleRouteLength', () => {
-  const from = new City('from');
-  const to = new City('to');
-  const final = new City('final');
-  const connection1 = new Connection(from.name, to.name, 3, TrackColor.Red);
-  const connection2 = new Connection(to.name, final.name, 4, TrackColor.Black);
+  const connection1 = new Connection('from', 'to', 3, TrackColor.Red);
+  const connection2 = new Connection('to', 'final', 4, TrackColor.Black);
   const route = new Route([connection1, connection2]);
 
   expect(route.actionLength()).toBe(2);
 });
 
 test('doubleRouteTrainLength', () => {
-  const from = new City('from');
-  const to = new City('to');
-  const final = new City('final');
-  const connection1 = new Connection(from.name, to.name, 3, TrackColor.Red);
-  const connection2 = new Connection(to.name, final.name, 4, TrackColor.Black);
+  const connection1 = new Connection('from', 'to', 3, TrackColor.Red);
+  const connection2 = new Connection('to', 'final', 4, TrackColor.Black);
   const route = new Route([connection1, connection2]);
 
   expect(route.trainLength()).toBe(7);
 });
 
 test('invalidRoute', () => {
-  const from = new City('from');
-  const to1 = new City('to1');
-  const to2 = new City('to2');
-  const final = new City('final');
-  const connection1 = new Connection(from.name, to1.name, 3, TrackColor.Red);
-  const connection2 = new Connection(to2.name, final.name, 4, TrackColor.Black);
+  const connection1 = new Connection('from', 'to1', 3, TrackColor.Red);
+  const connection2 = new Connection('to2', 'final', 4, TrackColor.Black);
 
   expect(() => {
     new Route([connection1, connection2]);
@@ -57,31 +42,23 @@ test('invalidRoute', () => {
 });
 
 test('validRoute', () => {
-  const from = new City('from');
-  const to = new City('to');
-  const final = new City('final');
-  const connection1 = new Connection(from.name, to.name, 3, TrackColor.Red);
-  const connection2 = new Connection(to.name, final.name, 4, TrackColor.Black);
+  const connection1 = new Connection('from', 'to', 3, TrackColor.Red);
+  const connection2 = new Connection('to', 'final', 4, TrackColor.Black);
   const route = new Route([connection1, connection2]);
 
   expect(route).toBeTruthy();
 });
 
 test('validRouteRever', () => {
-  const from = new City('from');
-  const to = new City('to');
-  const final = new City('final');
-  const connection1 = new Connection(from.name, to.name, 3, TrackColor.Red);
-  const connection2 = new Connection(to.name, final.name, 4, TrackColor.Black);
+  const connection1 = new Connection('from', 'to', 3, TrackColor.Red);
+  const connection2 = new Connection('to', 'final', 4, TrackColor.Black);
   const route = new Route([connection2, connection1]);
 
   expect(route).toBeTruthy();
 });
 
 test('route', () => {
-  const from = new City('from');
-  const to = new City('to');
-  const connection = new Connection(from.name, to.name, 3, TrackColor.Red);
+  const connection = new Connection('from', 'to', 3, TrackColor.Red);
   const route = new Route([connection]);
 
   expect(route.trainLength()).toBe(3);
--- src/model/__tests__/ticket.test.ts
+++ src/model/__tests__/ticket.test.ts
@@ -1,4 +1,3 @@
-import { City } from 'model/city';
 import { Connection } from 'model/connection';
 import { Route } from 'model/route';
 import { Ticket } from 'model/ticket';
@@ -12,26 +11,19 @@ test('mission', () => {
 });
 
 test('routeCompletesMission', () => {
-  const from = new City('from');
-  const to = new City('to');
-  const final = new City('final');
-  const connection1 = new Connection(from.name, to.name, 3, TrackColor.Red);
-  const connection2 = new Connection(to.name, final.name, 4, TrackColor.Black);
+  const connection1 = new Connection('from', 'to', 3, TrackColor.Red);
+  const connection2 = new Connection('to', 'final', 4, TrackColor.Black);
   const route = new Route([connection1, connection2]);
-  const mission = new Ticket(from.name, final.name, 10);
+  const mission = new Ticket('from', 'final', 10);
 
   expect(mission.completedBy(route)).toBe(true);
 });
 
 test('routeNotCompletesMission', () => {
-  const from = new City('from');
-  const to = new City('to');
-  const final = new City('final');
-  const connection1 = new Connection(from.name, to.name, 3, TrackColor.Red);
-  const connection2 = new Connection(to.name, final.name, 4, TrackColor.Black);
+  const connection1 = new Connection('from', 'to', 3, TrackColor.Red);
+  const connection2 = new Connection('to', 'final', 4, TrackColor.Black);
   const route = new Route([connection1, connection2]);
-  const notInRoute = new City('not');
-  const mission = new Ticket(from.name, notInRoute.name, 10);
+  const mission = new Ticket('from', 'not', 10);
 
   expect(mission.completedBy(route)).toBe(false);
 });
